a92a5defdd555ee22f7851dbcd71be764e2cd4e1
Vinh Nguyen <vinhnv33@onemount.com>
2020-11-14 03:26:59 +0700

    v2

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 const compression = require('compression')
-app.use(compression());
+// app.use(compression());
 app.use(express.static('build'));
 
 const listener = app.listen(process.env.PORT || 3000, function () {
